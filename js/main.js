25cfc90c95e6f06a8a2039e68dc0886969601308
Hatim-Gw <m09181014@it.misuratau.edu.ly>
2024-07-06 20:14:36 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const dropLi = document.querySelector('.header .links ul');
+const mainLi = document.querySelector('.header .links');
+
+mainLi.onclick = ()=>{
+    console.log(1);
+     dropLi.classList.toggle('show');
+}
+/*hello*/
